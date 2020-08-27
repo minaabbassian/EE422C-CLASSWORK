e00c219ae25360fc1d17d4db0932892fcde091d0
mina abbassian <mina.abbassian@gmail.com>
2020-08-27 10:28:43 -0500

    added stuff

--- EE 422C CLASSWORK/src/lecture1/HelloWorld.java
+++ EE 422C CLASSWORK/src/lecture1/HelloWorld.java
@@ -2,12 +2,13 @@ package lecture1;
 
 public class HelloWorld {
 	public static void main (String[] args) {
-		if(args[0].contentEquals("Eng")) {
-			System.out.println("Hello World!");
-		} else {
-			System.out.println("Bonjour!");
+		if(args.length > 0) { //if no argument, then doesn't print anything
+			if(args[0].contentEquals("Eng")) {
+				System.out.println("Hello World!");
+			} else {
+				System.out.println("Bonjour!");
+			}
 		}
-		
 		
 	}
 
