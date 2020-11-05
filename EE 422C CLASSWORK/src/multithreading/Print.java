25f6c781db1d804739f26cfa31c1e3ff84046c2d
mina abbassian <mina.abbassian@gmail.com>
2020-11-05 15:01:15 -0600

    finished quiz 8

--- EE 422C CLASSWORK/src/multithreading/Print.java
+++ EE 422C CLASSWORK/src/multithreading/Print.java
@@ -4,14 +4,17 @@ package multithreading;
 
 public class Print {
 
-    Object printer = new Object();
+	Object printer = new Object();
     Object paper = new Object();
 
     public Print() {
         Thread t1 = new Thread(new Task1());
         Thread t2 = new Thread(new Task2());
+        
         t1.start();
         t2.start();
+  
+        
         try {
             Thread.sleep(1000);
             System.out.println("There are " + Thread.activeCount() +
@@ -25,10 +28,10 @@ public class Print {
         public void run() {
             synchronized (printer) {
                 System.out.println("Got printer");
+            }
                 synchronized (paper) {
                     System.out.println("Got paper also; now I can print.");
                 }
-            }
         }
     }
 
@@ -37,10 +40,10 @@ public class Print {
         public void run() {
             synchronized (paper) {
                 System.out.println("Got paper");
+            }
                 synchronized (printer) {
                     System.out.println("Got printer also; now I can print.");
                 }
-            }
         }
     }
 
